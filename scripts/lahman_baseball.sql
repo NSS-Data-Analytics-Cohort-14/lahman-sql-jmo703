a0a8f6fe890fc8174b606e8920a86c6b05ef9ca0
Jimmy Morgan <jmorgan22153@yahoo.com>
2025-03-06 20:24:00 -0500

    Question 4 walkthrough

--- scripts/lahman_baseball.sql
+++ scripts/lahman_baseball.sql
@@ -68,8 +68,8 @@ David Price at the time had earned $81,851,296
 -- 4. Using the fielding table, group players into three groups based on their position: label players with position OF as "Outfield", those with position "SS", "1B", "2B", and "3B" as "Infield", and those with position "P" or "C" as "Battery". Determine the number of putouts made by each of these three groups in 2016.
 
 SELECT
-	yearid
-	,SUM(po)
+	yearid AS year_id
+	,SUM(po) AS putouts
 		,CASE
 			WHEN pos = '1B' THEN 'infield'
 			WHEN pos = '2B' THEN 'infield'
