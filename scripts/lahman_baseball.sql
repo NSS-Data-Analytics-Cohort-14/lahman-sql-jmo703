dfccf1f6aa4f8fa8c8fa09b3fcde80655a00332b
Jimmy Morgan <jmorgan22153@yahoo.com>
2025-03-06 21:48:27 -0500

    Question 8 walkthrough

--- scripts/lahman_baseball.sql
+++ scripts/lahman_baseball.sql
@@ -313,17 +313,45 @@ FROM
 
 -- 8. Using the attendance figures from the homegames table, find the teams and parks which had the top 5 average attendance per game in 2016 (where average attendance is defined as total attendance divided by number of games). Only consider parks where there were at least 10 games played. Report the park name, team name, and average attendance. Repeat for the lowest 5 average attendance.
 
-SELECT 
+(SELECT 
 	p.park_name
 	, h.attendance / 81 AS avg_attendance
-	, h.team
+	, t.name
+	, 'top 5'
 FROM 
 	homegames AS h
 LEFT JOIN
 	parks AS p
 ON h.park = p.park
+LEFT JOIN
+	teams AS t
+ON h.team = t.teamid
+AND h.year = t.yearid
 WHERE year = 2016
+AND h.games > 10
 ORDER BY h.attendance DESC
+limit 5
+)
+UNION
+
+(SELECT 
+	p.park_name
+	, h.attendance / 81 AS avg_attendance
+	, t.name
+	, 'bottom 5'
+FROM 
+	homegames AS h
+LEFT JOIN
+	parks AS p
+ON h.park = p.park
+LEFT JOIN
+	teams AS t
+ON h.team = t.teamid
+AND h.year = t.yearid
+WHERE year = 2016
+AND h.games > 10
+ORDER BY h.attendance DESC
+limit 5)
 
 SELECT 
 	DISTINCT (h.team)
